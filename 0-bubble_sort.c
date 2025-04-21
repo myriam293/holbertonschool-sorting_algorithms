5554b76a68d9b823521ac4630fedb20cbf62422c
Myriam Hajjaji <myriamhajjaji@Myriams-MacBook-Pro.local>
2025-04-21 21:57:29 +0100

    task 0 compiled

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,7 +2,8 @@
 #include "sort.h"
 
 /**
- * bubble_sort - A function that sorts an array of integers in ascending order using the Bubble sort algorithm.
+ * bubble_sort - A function that sorts an array of
+ * integers in ascending order using the Bubble sort algorithm.
  *
  * @array: The array to sort.
  * @size: The length of the array.
